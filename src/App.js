fa5b3b3bdae28ce417d717878772252965950392
Matteo Pugliese <stenwie91@hotmail.it>
2020-01-07 16:42:29 +0100

    ADD prettier to prj, ADD font to title and background color

--- src/App.js
+++ src/App.js
@@ -11,31 +11,39 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
     };
+
+    // this.handleChange = this.handleChange.bind(this); -> without Arrow Function !!!
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  render () {
+  // handleChange(e) {
+  //   this.setState({ searchField: e.target.value }, () =>
+  //     console.log(this.state) // questa è una callback -> è importante per dare sincronicità ad una funzione !!!
+  // )}
+
+  handleChange = (event) => {
+    this.setState(
+      { searchField: event.target.value } // => il 'Context' è tutto App.js Component !!!
+    );
+  };
+
+  render() {
     const { monsters, searchField } = this.state; // questa cosa è chiamata 'destrutturazione' o 'Destructuring' !!!
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase()) // metodo per filtrare la ricerca!!
+    const filteredMonsters = monsters.filter(
+      (monster) => monster.name.toLowerCase().includes(searchField.toLowerCase()) // metodo per filtrare la ricerca !!!
     );
 
     return (
       <div className="App">
-        <SearchBox
-          placeholder='Search Monsters' 
-          handleChange={(e) => {
-            this.setState({ searchField: e.target.value }, () => 
-              console.log(this.state) // questa è una callback -> è importante per dare sincronicità ad una funzione!!!
-            )}}
-        />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-export const SearchBox = ({ placeholder, handleChange }) => ( // questo è un 'Functional Component' !!!
-    <input
-        className='search'
-        type='search' 
-        placeholder={placeholder} 
-        onChange={handleChange}
-    />
-)
+export const SearchBox = (
+  { placeholder, handleChange } // questo è un 'Functional Component' !!!
+) => <input className="search" type="search" placeholder={placeholder} onChange={handleChange} />;
