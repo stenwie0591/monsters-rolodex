a2fc9accf5ba0e9235c940aa64e2a91aa00f8074
Matteo Pugliese <stenwie91@hotmail.it>
2020-01-07 16:54:40 +0100

    Updates

---
+++ precache-manifest.910ec620c185fee1c69205b6ea4693c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b15aa81790f592e18c65c205e2a3953",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4e2768803ed84002f78",
+    "url": "/monsters-rolodex/static/css/main.6dbb0b71.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4e2768803ed84002f78",
+    "url": "/monsters-rolodex/static/js/main.a6eb54f2.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
